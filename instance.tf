0ac107bab25dc3745cdc32d2608961820cffcbbb
Akshada Kanawade <kanawadeakshada23@gmail.com>
2025-04-05 15:30:58 +0530

    my ec2 instance

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"     
 }
 resource "aws_instance" "myec2" {
-    ami ="ami-0274f4b62b6ae3bd5"
+    ami ="ami-0100e595e1cc1ff7f"
     key_name = "ohio-key"
     instance_type = "t2.micro"
     vpc_security_group_ids = [ "mysg1" ]
