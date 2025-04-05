4076249793368578abd868ab6a495943a522c959
Akshada Kanawade <kanawadeakshada23@gmail.com>
2025-04-05 15:27:30 +0530

    my ec2 instance

---
+++ instance.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "us-east-2"     
+}
+resource "aws_instance" "myec2" {
+    ami ="ami-0274f4b62b6ae3bd5"
+    key_name = "ohio-key"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ "mysg1" ]
+    tags = {
+      Name = "tag ak"
+    }
+  
+}
+# provider "aws" {
+#     region = "us-east-1"
+  
+# }
+# resource "aws_instance" "myec2" {
+#     ami = "ami-0678bf8844b66f72a"
+#     key_name = "demo"
+#     instance_type = "t2.micro"
+#     security_groups = ["sg"]
+#     tags = {
+#       Nmae = "Spiderma_instance"
+#     }
+  
+# }
