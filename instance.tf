0de39385a92f2d8e5a45d1fa91e39e25f9734c3c
Akshada Kanawade <kanawadeakshada23@gmail.com>
2025-04-06 14:43:01 +0530

    my vpc

--- instance.tf
+++ instance.tf
@@ -1,13 +1,20 @@
-  provider "aws" {
-  region = "us-east-1" 
+# Default provider (do not duplicate in vpc.tf)
+provider "aws" {
+  region = "us-east-1"
 }
-resource "aws_instance" "myec2" {
-  ami = "ami-01816d07b1128cd2d"
-  key_name = "id_rsa"
-  instance_type =  "t2.micro"
-  security_groups = [ "mysg1" ]
-  subnet_id = aws_subnet.pub_sub.id
+
+resource "aws_instance" "myec2_instance" {
+  ami             = "ami-01816d07b1128cd2d"
+  instance_type   = "t2.micro"
+  key_name        = "id_rsa"
+  security_groups = ["mysg1"]
+  subnet_id       = aws_subnet.pub_sub.id
+
   tags = {
     Name = "spiderman instance"
   }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.myec2_instance.public_ip
 }
